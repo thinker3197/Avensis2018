0c0805f10938ec64c63ef01b38dc4636dad4e395
thinker3197 <ashishgupta.3197@gmail.com>
2018-01-02 10:26:26 +0530

    Add timer update logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 const App = {
+  activateTimer(date) {
+    const endDate = new Date("Jan 22, 2018 12:00:00").getTime(),
+      $timer = document.getElementById("js-timer");
+
+    let days, hours, minutes;
+
+    const intervalId = setInterval(() => {
+      const currentTime = new Date().getTime(),
+        distance = endDate - currentTime;
+
+      days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+      const timeLeft = `${days} days : ${hours} hours : ${minutes} minutes`;
+
+      if (timeLeft !== $timer.innerHTML) {
+        $timer.innerHTML = timeLeft;
+      }
+
+      if (distance < 0) {
+        clearInterval(intervalId);
+
+        $timer.innerHTML = "0 days : 0 hours : 0 minutes";
+      }
+    }, 1000);
+  },
   setState(state) {
     // TODO - Add router and use state to show history changes
     window.location.hash = state;
@@ -42,6 +69,7 @@ const App = {
   init() {
     this.addEventListeners();
     this.setState("");
+    this.activateTimer();
   }
 }
 
