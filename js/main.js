6c01e4dab256a52b582dd6f6bed24ba8b7200189
thinker3197 <ashishgupta.3197@gmail.com>
2018-01-04 20:58:29 +0530

    Implement slider in message section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 const App = {
+  activateSlider() {
+    const content = [{
+      auth: 'Prof. KP Chaudhary',
+      msg: 'I am delighted to announce the commencement of AVENSIS 2017.Due to the ingenuity and vivacity of our students this fest has attained a marked grandeur with time so much so that it now seems to embody the pride of our institution'
+    }, {
+      auth: 'Dr. Rinky Dwivedi',
+      msg: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries'
+    }];
+
+    const $msgText = document.getElementById('js-message-txt'),
+      $msgAuth = document.getElementById('js-message-auth'),
+      $slider = document.getElementById('js-slider');
+
+    $slider.addEventListener('click', (e) => {
+      const index = +e.target.dataset.index;
+
+      $msgText.innerHTML = content[index].msg;
+      $msgAuth.innerHTML = '- ' + content[index].auth;
+
+      $slider.children[index].classList.add('active');
+      $slider.children[index !== 0 ? 0 : 1].classList.remove('active');
+    });
+  },
   activateTimer(date) {
     const endDate = new Date("Jan 10, 2018 00:00:00").getTime(),
       $timer = document.getElementById("js-timer");
@@ -27,13 +50,17 @@ const App = {
     }, 1000);
   },
   setState(state) {
-    const pos = document.getElementById('js-'+ state +'-section').offsetTop || 0;
+    const pos = document.getElementById('js-' + state + '-section').offsetTop || 0;
 
     window.location.hash = state;
 
-    window.scroll({top: pos, left: 0, behaviour: "smooth"});
+    window.scroll({
+      top: pos,
+      left: 0,
+      behaviour: "smooth"
+    });
   },
-  addEventListeners() {
+  activateNavbar() {
     const $navMenu = document.getElementById("js-nav-menu"),
       $navList = document.getElementById("js-nav-list"),
       $navWrapper = document.getElementById("js-nav-wrapper");
@@ -70,8 +97,9 @@ const App = {
     });
   },
   init() {
-    this.addEventListeners();
+    this.activateNavbar();
     this.activateTimer();
+    this.activateSlider();
   }
 }
 
